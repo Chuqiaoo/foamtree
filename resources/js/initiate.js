0eb7549800862f840991aef1576a6700d50a8748
Chuqiaoo <gongchuqiao@gmail.com>
2019-05-20 14:25:34 +0100

    remove url in token

--- resources/js/initiate.js
+++ resources/js/initiate.js
@@ -116,8 +116,7 @@ function foamtreeAnalysis(analysisParam){
     }
     $.ajax({
         // TODO PARSE FILTER PARAM -- FILTER=pValue:0.88$species:9606
-        url: "https://dev.reactome.org/AnalysisService/token/" + analysisParam + "/filter/species/"+ speciesIdFromUrl +"?sortBy=ENTITIES_PVALUE&order=ASC&resource=TOTAL",
-      //  url: "/AnalysisService/token/" + analysisParam + "/filter/species/"+ speciesIdFromUrl +"?sortBy=ENTITIES_PVALUE&order=ASC&resource=TOTAL",
+        url: "/AnalysisService/token/" + analysisParam + "/filter/species/"+ speciesIdFromUrl +"?sortBy=ENTITIES_PVALUE&order=ASC&resource=TOTAL",
         dataType: "json",
         type: "GET",
         beforeSend:  function() {
